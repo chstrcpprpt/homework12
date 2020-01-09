c4e58e95ecf268f545f0a48ea956abed4946f778
chstrcpprpt <cjhallen@gmail.com>
2020-01-09 14:00:11 +1100

    added api request module

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const apiRouter = require("./routes");
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/routeName", apiRouter); // change route name
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

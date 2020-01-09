7ae43916831daf093fce52f080b01a7698279ae4
chstrcpprpt <cjhallen@gmail.com>
2020-01-09 15:42:40 +1100

    added start of methods for insert, update & delete

--- server/db/index.js
+++ server/db/index.js
@@ -10,6 +10,7 @@ const connection = mysql.createConnection({
 
 let companydb = {};
 
+// Create a db method that will return all employees from the table
 companydb.all = () => {
   return new Promise((reslove, reject) => {
     connection.query(`SELECT * FROM employee`, (err, results) => {
@@ -21,6 +22,7 @@ companydb.all = () => {
   });
 };
 
+// Create a db method that will return a single employee from the table
 companydb.one = (id) => {
   return new Promise((reslove, reject) => {
     connection.query(`SELECT * FROM employee WHERE id = ?`, [id], (err, results) => {
@@ -31,5 +33,42 @@ companydb.one = (id) => {
     });
   });
 }
+
+// Create a db method that creates a new employee
+// edit "edit here"
+companydb.insert = () => {
+  return new Promise((reslove, reject) => {
+    connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ?`, ["edit here"], (err, results) => {
+      if(err) {
+        return reject(err);
+      }
+      return reslove(results);
+    });
+  });
+};
+
+// Create a db method that updates an employees role
+companydb.updateRole = (id) => {
+  return new Promise((reslove, reject) => {
+    connection.query(`UPDATE employee SET role_id WHERE id = ?`, [id], (err, results) => {
+      if(err) {
+        return reject(err);
+      }
+      return reslove(results);
+    });
+  });
+};
+
+// Create a db method that will delete an employee from the table
+companydb.delete = (id) => {
+  return new Promise((reslove, reject) => {
+    connection.query(`DELETE FROM employee WHERE id = ?`, [id], (err, results) => {
+      if(err) {
+        return reject(err);
+      }
+      return reslove(results);
+    });
+  });
+};
 
 module.exports = companydb;

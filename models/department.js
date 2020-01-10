99e68b6e3e029dabd3fc6e037e1fbdf244ace40e
chstrcpprpt <cjhallen@gmail.com>
2020-01-11 07:30:55 +1100

    added department class

--- models/department.js
+++ models/department.js
@@ -1,1 +1,31 @@
 const connection = require("../server/db/connection");
+
+class department {
+  constructor() {
+
+  };
+
+  view() {
+    return new Promise((resolve, reject) => {
+      connection.query(`SELECT * FROM department`, (err, results) => {
+        if(err) {
+          return reject(err);
+        }
+        return resolve(results);
+      });
+    });
+  };
+
+  // need to edit the query
+  add() {
+    return new Promise((resolve, reject) => {
+      connection.query(`INSERT INTO department (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, ["edit here"], (err, results) => {
+        if(err) {
+          return reject(err);
+        }
+        return resolve(results);
+      });
+    });
+  };
+
+}
